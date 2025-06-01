82a2c5718800e613d83ec52e0d6ab572385ea5e1
Shadan <mohdshadan1316@gmail.com>
2025-06-01 17:00:05 +0530

    file update for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,10 @@ import userRoutes from "./routes/user.routes.js";
 import bookRoutes from "./routes/book.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
-import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
 
 // Middlewares
 app.use(
@@ -31,14 +28,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/book", bookRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/cart", cartRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
