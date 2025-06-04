3fbf168c553146d6d26ab41b9fc7096718cc23c0
Shadan <mohdshadan1316@gmail.com>
2025-06-04 14:25:34 +0530

    updated the cookeis logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -33,7 +33,7 @@ const registerUser = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
     }
@@ -72,7 +72,7 @@ const userLogin = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
     }
